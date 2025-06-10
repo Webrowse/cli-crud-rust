d37bc7e891fdaa0cca353240372f5509db2e47bf
Webrowse <great.adarsh@gmail.com>
2025-06-11 01:09:18 +0530

    Tasks are written in the local file

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
-use std::env;
 
 
 
-fn main() -> Result<(), String>{
+use std::{env, fs::{File, OpenOptions}, io::{self, Write}};
+fn main() -> io::Result<()>{
     let arg:Vec<String> = env::args().skip(1).collect();
-    let mut tasks = vec![];
     if arg[0] == "add".to_string(){
-        let task = arg[1..].join(" ");
-        tasks.push(task);
+        // let task = arg[1..].join(" ");
+        writing_tasks(arg[1..].join(" "))?;
     }
-    println!("{:?}",tasks);
+    // println!("New Task added: {:?}",tasks);
+    Ok(())
+}
+fn writing_tasks(task: String) -> io::Result<()>{
+    let file_read = File::open("Tasks.txt");
+    match file_read{
+        Ok(_file) => {
+            let mut file = OpenOptions::new().append(true).open("Tasks.txt")?;
+            file.write_all( task.as_bytes())?;
+            println!("File updated successfully: Tasks.txt");
+        },
+        Err(_e) => {
+            let mut file = File::create("Tasks.txt")?;
+            file.write_all(task.as_bytes())?;
+            println!("New File created successfully: Tasks.txt");
+        }
+    }
     Ok(())
 }
