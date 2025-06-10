ae177209a2d805f91d5c2b5bad4e4d6861b75ecc
Webrowse <great.adarsh@gmail.com>
2025-06-11 02:37:44 +0530

    safe handling of file

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,32 @@
 use std::{env, fs::{File, OpenOptions}, io::{self, Write}};
 fn main() -> io::Result<()>{
     let arg:Vec<String> = env::args().skip(1).collect();
-    if arg[0] == "add".to_string(){
-        // let task = arg[1..].join(" ");
+    if arg.len() > 0 && arg[0] == "add".to_string(){
         writing_tasks(arg[1..].join(" "))?;
     }
-    // println!("New Task added: {:?}",tasks);
     Ok(())
 }
 fn writing_tasks(task: String) -> io::Result<()>{
+    let mut byte_task = task.as_bytes().to_vec();
+    byte_task.extend_from_slice(b"\n");
     let file_read = File::open("Tasks.txt");
     match file_read{
         Ok(_file) => {
-            let mut file = OpenOptions::new().append(true).open("Tasks.txt")?;
-            file.write_all( task.as_bytes())?;
+            let mut file = OpenOptions::new()
+                .append(true)
+                .open("Tasks.txt")?;
+            
+            file.write_all( &byte_task)?;
             println!("File updated successfully: Tasks.txt");
         },
-        Err(_e) => {
+        Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
             let mut file = File::create("Tasks.txt")?;
             file.write_all(task.as_bytes())?;
             println!("New File created successfully: Tasks.txt");
+        },
+        Err(e) => {
+            eprintln!("Error opening file: {}", e);
+            return Err(e);
         }
     }
     Ok(())
