d27d667826995d8a76deac6920acc4de0dbbe49e
Webrowse <great.adarsh@gmail.com>
2025-06-13 07:43:39 +0530

    delete added, list attached after every operation.

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,27 @@ fn main() -> io::Result<()> {
     if arg.len() > 0 {
         if arg[0] == "add".to_string() {
             writing_tasks(arg[1..].join(" "))?;
+            println!("{}",list_tasks().expect("add hit issue"));
         }
         if arg[0] == "list".to_string() {
             let content = list_tasks()?;
-            for (index, value) in content.lines().filter(|x| !x.trim().is_empty()).enumerate() {
+            for (index, value) in content.lines()
+                .filter(|x| !x.trim().is_empty())
+                .enumerate() {
                 println!("{}. {}", index + 1, value.trim());
             }
+            
         }
         if arg[0] == "complete".to_string() {
             complete(arg[1].clone())?;
+            println!("{}",list_tasks().expect("Complete ran into trouble"));
         }
+        
+        if arg[0] == "delete".to_string() {
+            delete(arg[1].clone())?;
+            println!("{}",list_tasks().expect("delete ran into trouble"));
+        }
+
     }
     Ok(())
 }
@@ -68,7 +79,7 @@ fn complete(task_str: String) -> io::Result<()> {
     let mut lines: Vec<String> = file_content.lines().map(|line| line.to_string()).collect();
     // Each line of file_content was collected as an separate element in a vector names "lines".
 
-    let task_found_count; //Because undex starts with 0, and list starts with 1
+    let mut task_found_count ; //Because undex starts with 0, and list starts with 1
 
     for (i, line) in lines.iter_mut().enumerate() {
         //iter through the Vector with tasks
@@ -81,18 +92,61 @@ fn complete(task_str: String) -> io::Result<()> {
                 if line.contains("[ ]") {
                     *line = line.replace("[ ]", "[X]");
                     println!("Task {} marked as complete", task_found_count);
+                    break;
                 } else {
                     *line = line.replace("[X]", "[ ]");
                     println!("Task {} marked as incomplete", task_found_count);
+                    break;
                 }
             }
         }
-        break;
     }
-    let new_content = lines.join("\n");
-
+    let content = lines.join("\n");
+    let new_content_n = format!("{}\n",content);
     let mut file = File::create("Tasks.txt")?;
-    file.write_all(new_content.as_bytes())?;
+    file.write_all(new_content_n.as_bytes())?;
+
+    Ok(())
+}
+
+fn delete(del:String) -> io::Result<()>{
+    let del_target = match del.parse::<usize>(){
+        Ok(a)  => a,
+        Err(e) => {
+            eprintln!("Invalid delete input: {}",e);
+            return Ok(());
+        }
+    };
+    let file_content = match list_tasks(){
+        Ok(content) => content,
+        Err(e) => {
+            eprintln!("Error while fetching file_content in Delete fn, Error: {}",e);
+            return Err(e);
+        }
+    };
+    let lines: Vec<String> = file_content.lines().map(|x|x.to_string()).collect();
+    let mut current_task:usize = 0;
+    let mut new_list: Vec<String> = Vec::new();
+    for line in lines.into_iter(){
+        current_task += 1;
+
+        if del_target == current_task {
+            println!("Deleted {}: {}",del_target, line);
+        }else{
+            new_list.push(line);
+        }
+    }
+
+    let new_content = new_list.join("\n");
+    let new_content_n = format!("{}\n",new_content);
+    let mut file = File::create("Tasks.txt")?;
+    file.write_all(new_content_n.as_bytes())?;
+
+
+
+
+
+
 
     Ok(())
 }
